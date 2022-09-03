3f9c5245a67ca207ec73a0a0c672f242b159f706
Maciej Krzywicki <maciej.ofc.k@gmail.com>
2022-09-03 12:17:48 +0200

    adding project

---
+++ bankapp/src/test/java/pl/maciejowsky/bankapp/BankappApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.maciejowsky.bankapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
